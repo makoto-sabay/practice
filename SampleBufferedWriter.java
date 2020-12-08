2c14e4506b20dc65c606849ed06adae46cd61850
makoto-sabay <64566189+makoto-sabay@users.noreply.github.com>
2020-12-09 03:45:58 +0900

    SampleBufferedWriter.java
    Practice 12

---
+++ SampleBufferedWriter.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
+public class SampleBufferedWriter {
+	public static void main(String[] args) {
+		File file = new File("C:\\Users\\Makoto\\eclipse-workspace\\sample\\test.txt");
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		
+		try {
+			if(!file.exists()) {
+				file.createNewFile();
+			}
+	        
+			fw = new FileWriter(file.getAbsoluteFile());
+			bw = new BufferedWriter(fw);
+	    bw.write(file.getName());
+	    bw.close();
+	    fw.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+}
